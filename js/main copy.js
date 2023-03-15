e9faab1bdb6aa6abd1cc2b26079f71be6ec2cc5a
Evgeniy <evgeniy8086@ya.ru>
2023-03-16 02:50:27 +0300

    + validate in submit function + promise

--- js/main copy.js
+++ js/main copy.js
@@ -47,84 +47,25 @@ function commentFormTextKeydown(event) {
 //-----------------------------
 // Отправка формы добавления сообщения
 function submitCommentForm(form) {
-
-    let valid = true;
-
-    let name = form.name.value.trim();
-    {
-        let errorStr = '';
-        if (name == '') {
-            valid = false;
-            errorStr = 'Имя пустое';   
-        }
-        else 
-        if (!/^[\w\p{sc=Cyrillic}][\w\p{sc=Cyrillic}\-\s\.]+$/ui.test(name)) {
-            valid = false;
-            errorStr = 'Неправильный формат имени'; 
-        }
-
-        if (errorStr != '') {
-            form.name.classList.add('input-text--error');
-            form.querySelector('[name="name-msgerror"]').innerHTML = errorStr;
-        }
-    }
-
-    let text = form.text.value.trim();
-    {
-        let errorStr = '';
-        if (text == '') {
-            valid = false;
-            errorStr = 'Текст пустой';
-        }
-        
-        if (errorStr != '') {
-            form.text.classList.add('textarea--error');
-            form.querySelector('[name="text-msgerror"]').innerHTML = errorStr;
-        }
-    }
-
-    let date = null;
-    {
-        let dateStr = form.date.value.trim();
-        let errorStr = '';
-        if (dateStr != '') {
-            let error;
-            ({error, date} = parseDate(dateStr));
-            if (error != 0) {
-                valid = false;
-                switch (error) {
-                    case 1: errorStr += 'Неправильный формат даты'; break;
-                    case 2: errorStr += 'Неправильная дата'; break;
-                } 
-            }
-        }
-
-        if (errorStr != '') {
-            form.date.classList.add('input-text--error');
-            form.querySelector('[name="date-msgerror"]').innerHTML = errorStr;
-        }
-    }
-
-    if (!valid) return;
+    let {valid, name, text, date} = validCommentForm(form);
     
-    let now = (new Date()).getTime();
-    let id = now + '-' + randomInt(1000000, 9999999); // уникальный id сообщения
-    if (!date) date = now; 
+    if (valid) {
+        let now = (new Date()).getTime();
+        let id = now + '-' + randomInt(1000000, 9999999); // уникальный id сообщения
+        if (!date) date = now; 
 
-    let comment = {
-        id: id,
-        name: name,
-        text: text,
-        date: date,
-        liked: false,
-    };
+        let comment = {
+            id: id,
+            name: name,
+            text: text,
+            date: date,
+            liked: false,
+        };
 
-    addComment(comment);
+        addComment(comment);
 
-    // очистка полей формы после отправки
-    //form.name.value = '';
-    form.text.value = '';
-    form.date.value = '';
+        clearCommentForm(form);
+    }
 }
 
 //-----------------------------
@@ -187,6 +128,82 @@ function countInputText(elem) {
 }
 
 //-----------------------------
+// Валидация формы добавления комментария. Возвращает объект с набором данных
+function validCommentForm(form) {
+    let valid = true;
+
+    let name = form.name.value.trim();
+    {
+        let errorStr = '';
+        if (name == '') {
+            valid = false;
+            errorStr = 'Имя пустое';   
+        }
+        else 
+        if (!/^[\w\p{sc=Cyrillic}][\w\p{sc=Cyrillic}\-\s\.]+$/ui.test(name)) {
+            valid = false;
+            errorStr = 'Неправильный формат имени'; 
+        }
+
+        if (errorStr != '') {
+            form.name.classList.add('input-text--error');
+            form.querySelector('[name="name-msgerror"]').innerHTML = errorStr;
+        }
+    }
+
+    let text = form.text.value.trim();
+    {
+        let errorStr = '';
+        if (text == '') {
+            valid = false;
+            errorStr = 'Текст пустой';
+        }
+        
+        if (errorStr != '') {
+            form.text.classList.add('textarea--error');
+            form.querySelector('[name="text-msgerror"]').innerHTML = errorStr;
+        }
+    }
+
+    let date = null;
+    {
+        let dateStr = form.date.value.trim();
+        let errorStr = '';
+        if (dateStr != '') {
+            let error;
+            ({error, date} = parseDate(dateStr));
+            if (error != 0) {
+                valid = false;
+                switch (error) {
+                    case 1: errorStr += 'Неправильный формат даты'; break;
+                    case 2: errorStr += 'Неправильная дата'; break;
+                } 
+            }
+        }
+
+        if (errorStr != '') {
+            form.date.classList.add('input-text--error');
+            form.querySelector('[name="date-msgerror"]').innerHTML = errorStr;
+        }
+    }
+
+    return { 
+        'valid': valid, 
+        'name': name,
+        'text': text,
+        'date': date,
+    };
+}
+
+//-----------------------------
+// Очистка формы отправки комментария после отправки
+function clearCommentForm(form) {
+    form.name.value = '';
+    form.text.value = '';
+    form.date.value = '';
+}
+
+//-----------------------------
 // Вывод всех комментариев на страницу из хранилища
 function printComments() {
     let comments = JSON.parse(localStorage.getItem('comments')) ?? [];
@@ -202,24 +219,27 @@ function printComments() {
 //-----------------------------
 // Получение html комментария
 function commentToHTML(comment) {
-    
-    let HTML = `<div class="comment" data-comment-id="${escapeHTML(comment.id)}">`;
-    // имя
-    HTML += `<div class="comment__name">${escapeHTML(comment.name)}</div>`;
-    // текст
-    HTML += `<div class="comment__text">${escapeHTML(comment.text)}</div>`
-    // дата
-    let humanDateStr = humanDateFormat(comment.date);
-    HTML += `<div class="comment__date">${escapeHTML(humanDateStr)}</div>`;
-    // меню с кнопками
-    let likedClass = comment.liked ? ' comment__menu-item--like-liked' : '';
-    HTML += `<div class="comment__menu">`
-        + `<div class="comment__menu-item comment__menu-item--del"></div>`
-        + `<div class="comment__menu-item comment__menu-item--like${likedClass}"></div>`
-        + `</div>`;
-    HTML += `</div>`;
+    let html = '';
 
-    return HTML;
+    html += `<div class="comment" data-comment-id="${escapeHTML(comment.id)}">`;
+
+    html += `<div class="comment__name">${escapeHTML(comment.name)}</div>`;
+
+    html += `<div class="comment__text">${escapeHTML(comment.text)}</div>`;
+
+    let humanDateStr = humanDateFormat(comment.date);
+    html += `<div class="comment__date">${escapeHTML(humanDateStr)}</div>`;
+
+    // меню с кнопками
+    html += `<div class="comment__menu">`;
+    html += `<div class="comment__menu-item comment__menu-item--del"></div>`;
+    let likedClass = comment.liked ? ' comment__menu-item--like-liked' : '';
+    html += `<div class="comment__menu-item comment__menu-item--like${likedClass}"></div>`;
+    html += `</div>`;
+
+    html += `</div>`;
+
+    return html;
 }
 
 //-----------------------------
@@ -238,36 +258,31 @@ function addComment(comment) {
 //-----------------------------
 // Удаление комментария
 function delComment(commentId) {
-    // удаляем со страницы
+    // delete comment from storage
+    let comments = JSON.parse(localStorage.getItem('comments')) ?? [];
+    let index = comments.findIndex(x => (x.id == commentId));
+    comments.splice(index, 1);
+    localStorage.setItem('comments', JSON.stringify(comments));
+
+    // delete comment from page
     let commentsBox = document.getElementById('comments-box');
     commentsBox.querySelector(`[data-comment-id="${commentId}"]`)?.remove();
-
-    // удаляем из хранилища
-    let delPromise = new Promise((resolve, reject) => {
-        let comments = JSON.parse(localStorage.getItem('comments')) ?? [];
-        let index = comments.findIndex(x => (x.id == commentId));
-        comments.splice(index, 1);
-        localStorage.setItem('comments', JSON.stringify(comments));
-        resolve(true);
-    });
 }
 
 //-----------------------------
 // Лайк комментария
 function likeComment(commentId) {
-    // лайк на странице
+    // like comment to storage
+    let comments = JSON.parse(localStorage.getItem('comments')) ?? [];
+    let index = comments.findIndex(x => (x.id == commentId));
+    comments[index].liked = !comments[index].liked;
+    localStorage.setItem('comments', JSON.stringify(comments));
+
+    // like comment to page
     let commentsBox = document.getElementById('comments-box');
     commentsBox.querySelector(`[data-comment-id="${commentId}"]`)
         ?.querySelector(`.comment__menu-item--like`)
         ?.classList.toggle('comment__menu-item--like-liked');
-
-    // лайк в хранилище
-    let likePromise = new Promise((resolve, reject) => {
-        let comments = JSON.parse(localStorage.getItem('comments')) ?? [];
-        let index = comments.findIndex(x => (x.id == commentId));
-        comments[index].liked = !comments[index].liked;
-        localStorage.setItem('comments', JSON.stringify(comments));
-    });
 }
 
 //-----------------------------
@@ -315,7 +330,7 @@ function parseDate(str) {
         date.getHours() == hours && 
         date.getMinutes() == minutes 
     ) {
-        return { 'error': 0, 'date': date.getTime() }; // ok
+        return { 'error': 0, 'date': date.getTime() };
     }
     else {
         return { 'error': 2, 'date': null }; // дата не реальная
